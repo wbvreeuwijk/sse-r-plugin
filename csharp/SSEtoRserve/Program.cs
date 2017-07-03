ea789c6af9916c5258b2d5864e97287759500537
wbvreeuwijk <bas@reeuwijk.net>
2017-07-03 16:16:19 +0200

    Typoe

--- csharp/SSEtoRserve/Program.cs
+++ csharp/SSEtoRserve/Program.cs
@@ -5,8 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
-using System.Thread;
+using System.Threading;
 using NLog;
 using System.IO;
 
